2c2801b6c6a536bf1615ddc5102470b98a2ed715
Vijay Mathew <vijay@fractl.io>
2025-05-20 11:42:32 +0530

    init logger only on server

--- src/runtime/logger.ts
+++ src/runtime/logger.ts
@@ -1,22 +1,51 @@
-import DailyRotateFile from 'winston-daily-rotate-file';
-import winston from 'winston';
+import { isNodeEnv } from '../utils/runtime.js';
 
-const fileTransport = new DailyRotateFile({
-  level: 'debug',
-  filename: 'logs/app-%DATE%.log',
-  datePattern: 'YYYY-MM-DD',
-  maxSize: '20m',
-  maxFiles: '7d',
-});
+let DailyRotateFile: any;
+let winston: any
 
-const consoleTransport = new winston.transports.Console();
+if (isNodeEnv) {
+  // Only import Node.js modules in Node environment
+  // Using dynamic imports to avoid breaking browser bundling
+  await import('winston-daily-rotate-file').then(module => {
+    DailyRotateFile = module.default;
+  });
+  await import('winston').then(module => {
+    winston = module.default;
+  });
+}
 
-export const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `[${timestamp}] ${level}: ${message}`;
-    })
-  ),
-  transports: [fileTransport, consoleTransport],
-});
+export let logger: any;
+
+if (isNodeEnv) {
+  const fileTransport = new DailyRotateFile({
+    level: 'debug',
+    filename: 'logs/app-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    maxSize: '20m',
+    maxFiles: '7d',
+  });
+
+  const consoleTransport = new winston.transports.Console();
+
+  logger = winston.createLogger({
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.printf(({ timestamp, level, message }: any) => {
+        return `[${timestamp}] ${level}: ${message}`;
+      })
+    ),
+    transports: [fileTransport, consoleTransport],
+  });
+} else {
+  function mkLogger(tag: string): Function {
+    return (msg: string) => {
+      console.log(`${tag}: ${msg}`)
+    }
+  }
+  logger = {
+    debug: mkLogger('DEBUG'),
+    info: mkLogger('INFO'),
+    warn: mkLogger('WARN'),
+    error: mkLogger('ERROR')
+  }
+}
