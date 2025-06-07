e00d2d748f8770a4c3be7d6646f82952d198df76
Vijay Mathew <vijay@fractl.io>
2025-06-07 13:20:36 +0530

    attempt to create schema dynamically

--- src/runtime/orm.ts
+++ src/runtime/orm.ts
@@ -1,27 +1,63 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { EntitySchema, Table, TableColumnOptions } from "typeorm"
 import { defaultDataSource } from "./resolvers/sqldb/database.js"
 
-@Entity()
-export class User {
-    @PrimaryGeneratedColumn()
-    id: number = 0
+type User = {
+    id?: number,
+    firstName: string,
+    lastName: string,
+    isActive?: boolean
+}
 
-    @Column()
-    firstName: string = ''
+function makeUserClass() {
+    /*@Entity()
+    class User {
+        @PrimaryGeneratedColumn()
+        id: number = 0
 
-    @Column()
-    lastName: string = ''
+        @Column()
+        firstName: string = ''
 
-    @Column()
-    isActive: boolean = true
+        @Column()
+        lastName: string = ''
+
+        @Column()
+        isActive: boolean = true
+    }
+    return User*/
+    return new EntitySchema<User>({
+        name: "user",
+        columns: {
+            id: {
+                type: Number,
+                primary: true,
+                generated: true,
+            },
+            firstName: {
+                type: String,
+            },
+            lastName: {
+                type: String,
+            },
+            isActive: {
+                type: Boolean,
+                default: true
+            }
+        }
+    })
 }
 
 export async function CreateTestEntities() {
     if (defaultDataSource) {
-        const userRepository = defaultDataSource.getRepository(User)
-        const user = new User()
-        user.firstName = "Timber"
-        user.lastName = "Saw"
+        const UserSchema = makeUserClass()
+        /*const cols: TableColumnOptions[] = new Array()
+        cols.push({name: "id", isPrimary: true, type: "int", isGenerated: true})
+        cols.push({name: "firstName", type: "varchar"})
+        cols.push({name: "lastName", type: "varchar"})
+        cols.push({name: "isActive", type: "boolean", default: true})
+        const table: Table = new Table({name: 'user', columns: cols})*/
+        defaultDataSource.createQueryRunner().createTable(table)
+        const userRepository = defaultDataSource.getRepository(UserSchema)
+        const user = { firstName: "Timber", lastName: "Saw" }
         await userRepository.save(user)
 
         const allUsers = await userRepository.find()
