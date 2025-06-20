017dc319f46bcb008e72096e323f6fd70bdb1f8c
Vijay Mathew <vijay@fractl.io>
2025-06-20 11:06:33 +0530

    run agent-tests only locally

--- test/runtime/agents.test.ts
+++ test/runtime/agents.test.ts
@@ -4,12 +4,14 @@ import { AgentServiceProvider, AIResponse, humanMessage, systemMessage } from ".
 
 describe('Basic module operations', () => {
   test('check create module', async () => {
-    const ai: AgentServiceProvider = new (provider("OpenAI"))()
-    await ai.invoke([
+    if (process.env.AL_TEST) {
+      const ai: AgentServiceProvider = new (provider("OpenAI"))()
+      await ai.invoke([
         systemMessage("Is the following number odd? Answer YES or NO."),
         humanMessage("11")
-    ]).then((result: AIResponse) => {
+      ]).then((result: AIResponse) => {
         assert(result.content == "YES")
-    })
+      })
+    }
   })
 })
