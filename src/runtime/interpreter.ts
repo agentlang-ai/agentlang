6e0c7d17a338d76cde056e599b07dc6bafc42a1b
Vijay Mathew <73871437+vijayfractl@users.noreply.github.com>
2025-07-15 14:36:19 +0530

    fix agent code-gen (#151)

--- src/runtime/interpreter.ts
+++ src/runtime/interpreter.ts
@@ -33,7 +33,7 @@ import {
   getWorkflow,
   Instance,
   InstanceAttributes,
-  isAgentEvent,
+  isAgentEventInstance,
   isBetweenRelationship,
   isContainsRelationship,
   isEmptyWorkflow,
@@ -924,7 +924,7 @@ async function evaluateCrudMap(crud: CrudMap, env: Environment): Promise<void> {
       }
     }
   } else if (isEventInstance(inst)) {
-    if (isAgentEvent(inst)) await handleAgentInvocation(inst, env);
+    if (isAgentEventInstance(inst)) await handleAgentInvocation(inst, env);
     else await evaluate(inst, (result: Result) => env.setLastResult(result), env);
   } else {
     env.setLastResult(inst);
