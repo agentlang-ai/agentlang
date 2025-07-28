dbb9db861a87c7e51e48cca6d7ee42edbba12c94
Vijay Mathew <vijay@fractl.io>
2025-07-28 12:59:20 +0530

    load al-deps

--- src/runtime/loader.ts
+++ src/runtime/loader.ts
@@ -50,7 +50,6 @@ import {
   makeFqName,
   maybeExtends,
   registerInitFunction,
-  runShellCommand,
 } from './util.js';
 import { getFileSystem, toFsPath, readFile, readdir, exists } from '../utils/fs-utils.js';
 import { URI } from 'vscode-uri';
@@ -126,9 +125,8 @@ export async function extractDocument(
     );
 
     for (const validationError of validationErrors) {
-      const errorMsg = `line ${validationError.range.start.line + 1}: ${
-        validationError.message
-      } [${document.textDocument.getText(validationError.range)}]`;
+      const errorMsg = `line ${validationError.range.start.line + 1}: ${validationError.message
+        } [${document.textDocument.getText(validationError.range)}]`;
       if (isNodeEnv && chalk) {
         console.error(chalk.red(errorMsg));
       } else {
@@ -172,7 +170,7 @@ async function loadApp(appDir: string, fsOptions?: any, callback?: Function): Pr
       return;
     }
     directoryContents.forEach(file => {
-      if (path.extname(file) == '.al') {
+      if (path.extname(file).toLowerCase() == '.al') {
         alFiles.push(appDir + path.sep + file);
       }
     });
@@ -182,19 +180,17 @@ async function loadApp(appDir: string, fsOptions?: any, callback?: Function): Pr
     if (callback) await callback(appSpec);
   }
   if (appSpec.dependencies != undefined) {
-    if (isNodeEnv) {
-      // Only run shell commands in Node.js environment
-      for (const [depName, depVer] of Object.entries(appSpec.dependencies)) {
-        runShellCommand(`npm install ${depName}@${depVer}`, undefined, cont2);
+      for (const [depName, _] of Object.entries(appSpec.dependencies)) {
+        const depDirName = `./node_modules/${depName}`
+        const files = await fs.readdir(depDirName)
+        if (files.find(file => {
+          return (path.extname(file).toLowerCase() == '.al')
+        })) {
+          await loadApp(depDirName, fsOptions)
+        }
       }
-    } else {
-      // In non-Node environments, log a warning and continue
-      console.warn('Dependencies cannot be installed in non-Node.js environments');
-      await cont2();
     }
-  } else {
     await cont2();
-  }
   return appSpec.name || lastModuleLoaded;
 }
 
