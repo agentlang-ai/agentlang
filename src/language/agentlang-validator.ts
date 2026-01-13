ab7a39d163bdfaf89e367ea8eac692637b8eab15
Vijay Mathew <73871437+vijayfractl@users.noreply.github.com>
2026-01-13 09:40:45 +0530

    duplicate names and definitions are allowed (#520)
    * duplicate names and definitions are allowed
    
    * add formatted flow test

--- src/language/agentlang-validator.ts
+++ src/language/agentlang-validator.ts
@@ -1,11 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import {
-  AgentlangAstType,
-  AttributeDefinition,
-  isStandaloneStatement,
-  ModuleDefinition,
-  SchemaDefinition,
-} from './generated/ast.js';
+import { AgentlangAstType, AttributeDefinition, SchemaDefinition } from './generated/ast.js';
 import type { AgentlangServices } from './agentlang-module.js';
 
 /**
@@ -15,7 +9,6 @@ export function registerValidationChecks(services: AgentlangServices) {
   const registry = services.validation.ValidationRegistry;
   const validator = services.validation.AgentlangValidator;
   const checks: ValidationChecks<AgentlangAstType> = {
-    ModuleDefinition: validator.checkUniqueDefs,
     SchemaDefinition: validator.checkUniqueAttributes,
   };
   registry.register(checks, validator);
@@ -25,49 +18,6 @@ export function registerValidationChecks(services: AgentlangServices) {
  * Implementation of custom validations.
  */
 export class AgentlangValidator {
-  // our new validation function for defs
-  checkUniqueDefs(module: ModuleDefinition, accept: ValidationAcceptor): void {
-    // create a set of visited functions
-    // and report an error when we see one we've already seen
-    const reported = new Set();
-    module.defs.forEach(d => {
-      let n: string | undefined;
-      if (!isStandaloneStatement(d)) {
-        if (
-          d.$type === 'PublicWorkflowDefinition' ||
-          d.$type === 'PublicAgentDefinition' ||
-          d.$type === 'PublicEventDefinition'
-        ) {
-          n = d.def.name;
-        } else {
-          n = d.name;
-        }
-        if (
-          d.$type != 'WorkflowDefinition' &&
-          d.$type != 'FlowDefinition' &&
-          d.$type != 'PublicWorkflowDefinition' &&
-          d.$type != 'ScenarioDefinition' &&
-          d.$type != 'DirectiveDefinition' &&
-          d.$type != 'GlossaryEntryDefinition' &&
-          reported.has(n)
-        ) {
-          accept('error', `Definition has non-unique name '${n}'.`, {
-            node: d,
-            property: 'name',
-          });
-        }
-        if (
-          d.$type != 'FlowDefinition' &&
-          d.$type != 'ScenarioDefinition' &&
-          d.$type != 'DirectiveDefinition' &&
-          d.$type != 'GlossaryEntryDefinition'
-        ) {
-          reported.add(n);
-        }
-      }
-    });
-  }
-
   checkUniqueAttributes(def: SchemaDefinition, accept: ValidationAcceptor): void {
     // create a set of visited functions
     // and report an error when we see one we've already seen
